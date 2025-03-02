49024b4def03d7892eaa96f8ce1c8abf8aea64e6
kaseken <k.kaseda0925@gmail.com>
2025-03-02 12:45:22 +0900

    Update iOS app

--- KMPSampleIOSApp/KMPSampleIOSApp/utils/PriorityQueueUtil.swift
+++ KMPSampleIOSApp/KMPSampleIOSApp/utils/PriorityQueueUtil.swift
@@ -2,18 +2,11 @@ import Foundation
 import KMPSampleSwiftPackage
 
 enum PriorityQueueUtil {
-    private class PQItem {
-        let value: Int
-        init(value: Int) {
-            self.value = value
-        }
-    }
-
     static func pickMinimum(from array: [Int]) -> Int? {
-        let pq = PriorityQueue<PQItem>(
-            comparator: { a, b in Int32(a.value - b.value) }
+        let pq = PriorityQueue<NSNumber>(
+            comparator: { a, b in a.int32Value - b.int32Value }
         )
-        array.forEach { pq.push(PQItem(value: $0)) }
-        return pq.pop()?.value
+        array.forEach { pq.push(NSNumber(integerLiteral: $0)) }
+        return pq.pop()?.intValue
     }
 }
