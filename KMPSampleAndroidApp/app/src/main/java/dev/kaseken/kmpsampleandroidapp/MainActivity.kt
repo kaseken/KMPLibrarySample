f175a7a7513ed459c24b4f34077333b576297a4b
kaseken <k.kaseda0925@gmail.com>
2025-03-02 12:22:18 +0900

    fix: Use PriorityQueue

--- KMPSampleAndroidApp/app/src/main/java/dev/kaseken/kmpsampleandroidapp/MainActivity.kt
+++ KMPSampleAndroidApp/app/src/main/java/dev/kaseken/kmpsampleandroidapp/MainActivity.kt
@@ -12,36 +12,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.kaseken.kmpsampleandroidapp.ui.theme.KMPSampleAndroidAppTheme
+import dev.kaseken.kmpsampleandroidapp.utils.PriorityQueueUtil
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KMPSampleAndroidAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      KMPSampleAndroidAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          val array = createRandomIntArray(5)
+          val min = PriorityQueueUtil.pickMinimum(array)
+          Greeting(
+              text = "The minimum element in $array is $min.",
+              modifier = Modifier.padding(innerPadding))
         }
+      }
     }
+  }
+
+  private fun createRandomIntArray(size: Int, range: IntRange = -100..100): List<Int> {
+    return List(size) { Random.nextInt(range.first, range.last + 1) }
+  }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+  Text(text = text, modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    KMPSampleAndroidAppTheme {
-        Greeting("Android")
-    }
+  KMPSampleAndroidAppTheme { Greeting("Android") }
 }
